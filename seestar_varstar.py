ad1fc612b216068018e48043938a891c5050ce63
Stefan Keller <aegiskeller@gmail.com>
2025-03-12 09:50:37 +0000

    not working but tired

--- seestar_varstar.py
+++ seestar_varstar.py
@@ -8,6 +8,9 @@ import seestar_params as sp
 
 def seestar_run(targetName, coords, exptime):
     # Define the path to the seestar_run.py script
+    #using subprocess to add arguments
+    subprocess.run(['python', f'{}/seestar_run.py', sp.seestar_ip, targetName, str(coords[0]), str(coords[1]), '0', str(exptime), '1', '1', '1', '1'])
+
     seestar_run_path = f'{sp.path}/seestar_run.py {sp.seestar_ip} \'{targetName}\' {coords[0]} {coords[1]} 0 {exptime} 1 1 1 1'
     
     # Run the seestar_run.py script and check for errors
