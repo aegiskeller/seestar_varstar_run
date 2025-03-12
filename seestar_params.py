c8b7a9788b6028bf41876e7fe66f6792cdfa3554
Stefan Keller <aegiskeller@gmail.com>
2025-03-12 09:43:58 +0000

    path issue

--- seestar_params.py
+++ seestar_params.py
@@ -1,1 +1,2 @@
 seestar_ip = '192.168.1.35'
+path = '/c/Users/Admin/Documents/GitHub/seestar_varstar_run'
--- seestar_varstar.py
+++ seestar_varstar.py
@@ -8,7 +8,7 @@ import seestar_params as sp
 
 def seestar_run(targetName, coords, exptime):
     # Define the path to the seestar_run.py script
-    seestar_run_path = f'./seestar_run.py {sp.seestar_ip} \'{targetName}\' {coords[0]} {coords[1]} 0 {exptime} 1 1 1 1'
+    seestar_run_path = f'{sp.path}/seestar_run.py {sp.seestar_ip} \'{targetName}\' {coords[0]} {coords[1]} 0 {exptime} 1 1 1 1'
     
     # Run the seestar_run.py script and check for errors
     try:
